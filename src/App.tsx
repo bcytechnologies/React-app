9512f7fab93b2a0e92e54a1b3b69a905c32cd2de
Bryan Young <bryan@bcytechnologies.com>
2025-02-23 21:50:52 -0600

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Alert from "./components/Alert";
+
+function App() {
+  return (
+    <div>
+      <Alert>
+        <span>Bryan is home</span>
+      </Alert>
+    </div>
+  );
+}
+
+export default App;
