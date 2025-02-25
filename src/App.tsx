85b77bba3b972c425451b559e0a0d89db625815a
Bryan Young <bryan@bcytechnologies.com>
2025-02-24 22:33:38 -0600

    Working with CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 
 function App() {
+  const [alertVisible, alertVisability] = useState(false);
+  const items = ["Houston", "Dallas", "Dixon", "Arlington", "Palestine"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
-    <div>
-      <Alert>
-        <span>Bryan is home</span>
-      </Alert>
-    </div>
+    <ListGroup
+      items={items}
+      heading="Cities"
+      onSelectItem={handleSelectItem}
+    ></ListGroup>
   );
 }
 
